23b095885ba88f2709fe180c68d17bcbaa925ad6
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-02-22 22:32:33 +0000

    feat: Add day 1 walkthroughs

---
+++ values.go
@@ -0,0 +1,12 @@
+package main
+
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("2+8=", 2+8)
+	fmt.Println("Binary floating point 0.1 + 0.2", 0.1+0.2)
+	fmt.Println("Logical operations", true && false, true || false, !true)
+}
+
